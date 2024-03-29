63ad1602031d948b46a6249f5905bc243b54a28e
Evan <1181678438@qq.com>
2024-03-29 22:47:32 +0800

    3.29

--- src/BWDAO.sol
+++ src/BWDAO.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-
+import "./BWDERC20.sol";
 contract BWDAO {
     address public Owner;
+    address BWD = 0x408ea061DEF5E8E57Bd2569C9ca7d4Ce400860e0;
     mapping(address => bool) public Admin;
     mapping(address => bool) public memberlisted;
+    mapping(address => uint) public contributions;
 
     modifier onlyOwner {
         require(msg.sender == Owner, "You are not Owner");
@@ -27,6 +29,13 @@ contract BWDAO {
     function addMember(address _member) external onlyAdmin onlyOwner{
         require(memberlisted[msg.sender] == false ,"He has been a member");
         memberlisted[_member] = true;
+    }
+
+    function fundDAO() external payable{
+        require(msg.value > 0.01 ether, "Donations are too small");
+        require(memberlisted[msg.sender] == true, "You are not DAOMember");
+        contributions[msg.sender] += msg.value;
+        BWDERC20(BWD).mint(msg.sender, msg.value);
     }
 
 }
