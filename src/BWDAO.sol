36d039ea29ceb16dcadeee540d3ed35465d3c2d6
Evan <1181678438@qq.com>
2024-03-30 16:57:15 +0800

    token

--- src/BWDAO.sol
+++ src/BWDAO.sol
@@ -6,7 +6,6 @@ contract BWDAO {
     address BWD = 0x408ea061DEF5E8E57Bd2569C9ca7d4Ce400860e0;
     mapping(address => bool) public Admin;
     mapping(address => bool) public memberlisted;
-    mapping(address => uint) public contributions;
 
     modifier onlyOwner {
         require(msg.sender == Owner, "You are not Owner");
@@ -29,13 +28,6 @@ contract BWDAO {
     function addMember(address _member) external onlyAdmin onlyOwner{
         require(memberlisted[msg.sender] == false ,"He has been a member");
         memberlisted[_member] = true;
-    }
-
-    function fundDAO() external payable{
-        require(msg.value > 0.01 ether, "Donations are too small");
-        require(memberlisted[msg.sender] == true, "You are not DAOMember");
-        contributions[msg.sender] += msg.value;
-        BWDERC20(BWD).mint(msg.sender, msg.value);
     }
 
 }
--- src/BWDERC20.sol
+++ src/BWDERC20.sol
@@ -4,15 +4,28 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BWDERC20 is ERC20, ERC20Permit, ERC20Votes, Ownable {
-    constructor(address initialOwner)
+contract BWDERC20 is ERC20, ERC20Permit, ERC20Votes {
+    address public owner;
+    mapping(address => bool) public daoMembers;
+    mapping(address => uint) public contributions;
+
+    constructor()
         ERC20("BWinDAO", "BWD")
         ERC20Permit("BWinDAO")
-        Ownable(initialOwner)
     {
+        owner = msg.sender;
         _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+
+    modifier onlyDaoMembers {
+        require(daoMembers[msg.sender] == true, "You are not daoMember");
+        _;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not owner");
+        _;
     }
 
     function _update(address from, address to, uint256 value)
@@ -29,5 +42,16 @@ contract BWDERC20 is ERC20, ERC20Permit, ERC20Votes, Ownable {
         returns (uint256)
     {
         return super.nonces(owner);
+    }
+
+    function setDaoMembers(address _dao) external onlyOwner{
+        daoMembers[_dao] = true;
+    }
+
+    function mintDao() external payable onlyDaoMembers {
+        require(msg.value > 0.01 ether, "Donations are too small");
+        require(daoMembers[msg.sender] == true, "You are not DAOMember");
+        contributions[msg.sender] += msg.value;
+        _mint(msg.sender, msg.value);
     }
 }
